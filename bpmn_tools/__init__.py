ac363d68dbb7a9636922baf42262a7ac73c1016e
Christophe VG <contact@christophe.vg>
2023-05-27 16:33:55 +0200

    added classes member with all classes

--- bpmn_tools/__init__.py
+++ bpmn_tools/__init__.py
@@ -3,3 +3,20 @@
 """
 
 __version__ = "0.0.1"
+
+import sys, inspect
+
+from bpmn_tools.notation      import Definitions
+from bpmn_tools.collaboration import Collaboration, Participant
+from bpmn_tools.flow          import Process, Start, End, Task, Flow
+from bpmn_tools.diagrams      import Diagram, Plane, Shape, Edge
+
+def get_classes(module):
+  return [
+    c[1] for c in inspect.getmembers(sys.modules[module], inspect.isclass)
+  ]
+
+classes = get_classes("bpmn_tools.notation") + \
+          get_classes("bpmn_tools.collaboration") + \
+          get_classes("bpmn_tools.flow") + \
+          get_classes("bpmn_tools.diagrams")
