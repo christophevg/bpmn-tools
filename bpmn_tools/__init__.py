ea99d9139573be43a2768fd2fc3c67efd3e80869
Christophe VG <contact@christophe.vg>
2023-05-30 19:07:20 +0200

    clean up imports

--- bpmn_tools/__init__.py
+++ bpmn_tools/__init__.py
@@ -6,10 +6,7 @@ __version__ = "0.0.3"
 
 import sys, inspect
 
-from bpmn_tools.notation      import Definitions
-from bpmn_tools.collaboration import Collaboration, Participant
-from bpmn_tools.flow          import Process, Start, End, Task, Flow
-from bpmn_tools.diagrams      import Diagram, Plane, Shape, Edge
+from bpmn_tools import notation, collaboration, flow, diagrams
 
 def get_classes(module):
   return [
