fcaea7c230c5c20561374ea75104f4f185ed1b62
Christophe VG <contact@christophe.vg>
2023-09-15 13:46:06 +0200

    added test case for basic model checking

---
+++ tests/test_model.py
@@ -0,0 +1,15 @@
+from bpmn_tools.collaboration import Collaboration, Participant
+from bpmn_tools.flow          import Process, Start, End, Task
+from bpmn_tools.flow          import Flow
+
+def test_create_single_step_process():
+  task = Task('Say "Hello!"', id="hello")
+
+  activities = [ Start(id="start"), task, End(id="end") ]
+
+  process = Process(id="process").extend(activities).extend([
+    Flow(source=activities[0], target=activities[1]),
+    Flow(source=activities[1], target=activities[2])
+  ])
+
+  assert task.process == process
