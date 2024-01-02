a9177f445797e2cd5bbcf9d1589fd2cf189d80b8
Christophe VG <contact@christophe.vg>
2024-01-02 14:18:38 +0100

    added test for issue #2

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,4 +1,4 @@
-from bpmn_tools.flow          import Process, Flow, Lane
+from bpmn_tools.flow          import Process, Flow, MessageFlow, Lane
 from bpmn_tools.flow          import Task
 from bpmn_tools.flow          import ExclusiveGateway
 from bpmn_tools.collaboration import Collaboration, Participant
@@ -7,7 +7,7 @@ from bpmn_tools.diagrams      import Diagram, Plane
 from bpmn_tools.layout        import simple
 
 def test_issue_1(compare_model_to_file):
-  # minimal example to illustrate raise problem, based on submitted example
+  # minimal example to illustrate problem, based on submitted example
   # https://github.com/christophevg/bpmn-tools/issues/1
 
   a = Task("task a", id="a")
@@ -39,3 +39,36 @@ def test_issue_1(compare_model_to_file):
   simple.layout(definitions)
 
   compare_model_to_file(definitions, "issues/1.bpmn", save_to="issues/1-test.bpmn")
+
+def test_issue_2(compare_model_to_file):
+  # minimal example to illustrate problem, based on submitted example
+  # https://github.com/christophevg/bpmn-tools/issues/2
+  
+  task = Task("task a", id="a")
+
+  process1 = Process(id="process1").append(task)
+  process2 = Process(id="process2")
+  
+  participant1 = Participant("participant 1", process1, id="participant 1")
+  participant2 = Participant("participant 2", process2, id="participant 2")
+
+  # "correct" target would be the participant
+  participant1.append(MessageFlow(id="flow", source=task, target=participant2))
+
+  collaboration = Collaboration(id="Colaboration").extend([
+    participant1, participant2
+  ])
+
+  definitions = Definitions(id="definitions").extend([
+    process1,
+    process2,
+    collaboration
+  ])
+
+  definitions.append(
+    Diagram( id="diagram", plane=Plane(id="plane", element=collaboration) )
+  )
+
+  simple.layout(definitions)
+
+  compare_model_to_file(definitions, "issues/2.bpmn", save_to="issues/2-test.bpmn")
