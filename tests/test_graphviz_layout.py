7684b957d71da4c1f493265bf8a4dee0bc32bed6
Christophe VG <contact@christophe.vg>
2024-01-24 17:39:42 +0100

    skip graphviz on non-darwin (e.g. GitHub 😇)

--- tests/test_graphviz_layout.py
+++ tests/test_graphviz_layout.py
@@ -5,6 +5,12 @@ from bpmn_tools.layout import graphviz
 
 from tests.conftest import load_model_src
 
+import sys
+import pytest
+
+if not sys.platform.startswith("darwin"):
+  pytest.skip("skipping windows-only tests", allow_module_level=True)
+
 def test_graphviz_routing(compare_model_to_file):
   model = Definitions.from_dict(
     xmltodict.parse(load_model_src("graphviz-original.bpmn"))
